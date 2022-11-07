6626255cdf1a93a19d0ecde666355e01fe2e598f
ManuelMellado <58300200+ManuelMellado@users.noreply.github.com>
2022-11-07 15:25:32 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=3-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=32
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=manuel.mellado@uam.es
 module load anaconda/anaconda3
