102a4f93aa4f919704d5b59b316634426a074b4a
ManuelMellado <58300200+ManuelMellado@users.noreply.github.com>
2022-11-07 14:49:30 +0100

    Delete launch.sh

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=3-00:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=100
-#SBATCH --mem-per-cpu=8G
-#SBATCH --mail-type=FAIL,END
-#SBATCH --mail-user=manuel.mellado@uam.es
-
-srun python aviones.py
