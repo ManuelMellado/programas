8787ff3a38624783eb9d49689c1dcd017130e065
ManuelMellado <58300200+ManuelMellado@users.noreply.github.com>
2022-11-07 15:08:27 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=3-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=manuel.mellado@uam.es
