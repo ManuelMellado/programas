b1d1d6a1a610ae728214f72a736a3aad69e70bad
ManuelMellado <58300200+ManuelMellado@users.noreply.github.com>
2022-11-07 14:51:45 +0100

    Add files via upload

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=3-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=100
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=manuel.mellado@uam.es
+module load anaconda/anaconda3
+
+srun python aviones.py
